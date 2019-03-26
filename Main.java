0e93118f49ed769bf427f31a1dc1e4546a5041d2
VV <xvwarvx@gmail.com>
2019-03-26 14:24:30 +0300

    init

---
+++ Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import com.company.Pipi;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int a, b;
+        int sum;
+        Pipi p = new Pipi();
+
+        a = 5;
+        b = 10;
+        sum = a * b;
+
+        System.out.println("Блдядь! sum=" + sum);
+
+        p.kuku();
+    }
+}
---
+++ Pipi.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Pipi {
+    public void kuku(){
+        System.out.println("kuku");
+        gog();
+    }
+
+    private void gog(){
+        System.out.println("111");
+    }
+}
